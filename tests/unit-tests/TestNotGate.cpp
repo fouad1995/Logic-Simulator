4d4951eb89715037c0a0ab0c3480b370c7a5e9e2
Fouad Ayman <fouadayman58@gmail.com>
2021-01-29 19:30:40 +0200

    adding unit test and structure for spikes

---
+++ tests/unit-tests/TestNotGate.cpp
@@ -0,0 +1,40 @@
+#include <gtest/gtest.h>
+#include "../include/not/Not_Gate.h"
+
+//<! TODO(FOUAD) : apply more checks as NOT-GATE is "not" allowed to take more than one input 
+//<! but currently it will take one input if you gave it two inputs it will ignore the second one
+
+class TestNot {
+public:
+	TestNot(int input1) {
+		auto not = new not_gate();
+		not->set_in1(new Node("1", input1));
+		not->set_out(new Node("out"));//<! out will be set
+		not->Calculate_Out();
+		result = not ->get_out()->getvalue();
+	};
+
+	int get_result() {
+		return result;
+	}
+
+private:
+	int result;
+
+};
+
+TEST(Not, Test_1) {
+
+	TestNot not (1);
+	auto result = not .get_result();
+
+	ASSERT_EQ(0, result);
+}
+
+TEST(Not, Test_0) {
+
+	TestNot not (0);
+	auto result = not .get_result();
+
+	ASSERT_EQ(1, result);
+}
